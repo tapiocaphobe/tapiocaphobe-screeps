ddc7b9095c62516c6a9fc83ecc050a2e68031f3a
tapiocaphobe <tapiocaphobe@gmail.com>
2021-07-26 18:28:03 -0600

    queuemanager initial commit

---
+++ scripts/queuemanager.js
@@ -0,0 +1,29 @@
+
+class QueueManager {
+    _queue = [];
+    _hashmap = new Map();
+    constructor(objectkeyset, value)
+    {
+        for(var n in objectkeyset)
+        {
+            this._hashmap.set(n.name, value);
+        }
+    }
+    add()
+    {
+
+    }
+    update(key, ...newvalue)
+    {
+        switch(typeof this._hashmap.get(key))
+        {
+            case 'boolean':
+                this._hashmap.set(key, !this._hashmap.get(key));
+                console.log("queuemanager boolean");
+                break;
+            default:
+                console.log(typeof this._hashmap.get(key));
+                break;
+        }
+    }
+}
